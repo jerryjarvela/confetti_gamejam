52b944c4d17a2677a1d792911dc0737fcbf9d136
ab0214 <ab0214@student.jamk.fi>
2022-03-10 17:04:55 +0200

    added animation to player

--- data/scripts/Player.gd
+++ data/scripts/Player.gd
@@ -4,9 +4,12 @@ var walk_speed: float = 10
 var gravity: float = 1
 var jump_velocity: float = 25
 var velocity: Vector3
+onready var character_sprite: Spatial = $Character
+onready var animation_player: AnimationPlayer = $Viewport/Character/AnimationPlayer
 
 func _ready():
 	move_lock_z = true
+	animation_player.stop()
 
 func _process(delta):
 	var direction: Vector3 = Vector3.ZERO;
@@ -18,3 +21,10 @@ func _process(delta):
 		velocity.y -= gravity
 	velocity = move_and_slide(velocity)
 	velocity.z = 0
+	
+	# animation	
+	if (Input.get_action_strength("right") - Input.get_action_strength("left") != 0):
+		animation_player.play("Walking") # play animation
+		character_sprite.flip_h = Input.is_action_pressed("left") # flip the sprite if going left
+	else:
+		animation_player.stop()
